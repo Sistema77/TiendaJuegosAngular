7ccd1b56637215f6caa630ff42b8f7e721dee661
Jose Clase <josemanuelmorenoguerra96@gmail.com>
2023-11-14 09:37:14 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JuegosComponent } from './Vistas/juegos/juegos.component';
+import { UsuarioComponent } from './Vistas/usuario/usuario.component';
+import { AlquileresComponent } from './Vistas/alquileres/alquileres.component';
+import { ListadoComponent } from './Vistas/juegos/listado/listado.component';
+import { DetalleComponent } from './Vistas/juegos/detalle/detalle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "juegos", component : JuegosComponent, children : [
+    {path: "listado", component :ListadoComponent},
+    {path: "edit/:id", component : DetalleComponent},
+    {path: "detalle", component : DetalleComponent}
+  ]},
+  {path: "usuarios", component : UsuarioComponent},
+  {path: "alquileres", component : AlquileresComponent},
+  {path: "", redirectTo : "juegos", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlquileresComponent } from './Vistas/alquileres/alquileres.component';
+import { JuegosComponent } from './Vistas/juegos/juegos.component';
+import { UsuarioComponent } from './Vistas/usuario/usuario.component';
+import { ListadoComponent } from './Vistas/juegos/listado/listado.component';
+import { DetalleComponent } from './Vistas/juegos/detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlquileresComponent,
+    JuegosComponent,
+    UsuarioComponent,
+    ListadoComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
